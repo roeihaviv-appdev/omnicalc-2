05a680895d8567d825cf02d5cf64de24d6749a9f
roeihaviv <rhaviv@chicagobooth.edu>
2022-04-24 16:47:00 +0000

    Add + subtract + multiply completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,48 @@
 class ApplicationController < ActionController::Base
+  def blank_add_form
+
+    render({ :template => "math_templates/add_form.html.erb"})
+  end
+
+  def blank_add_results
+
+    @num1 = params.fetch("num1").to_f
+    @num2 = params.fetch("num2").to_f
+    @result_add = @num1 + @num2
+
+    render({ :template => "math_templates/add_results.html.erb"})
+  end
+
+  ###
+
+  def blank_subtract_form
+
+    render({ :template => "math_templates/subtract_form.html.erb"})
+  end
+
+  def blank_subtract_results
+
+    @num1 = params.fetch("num1").to_f
+    @num2 = params.fetch("num2").to_f
+    @result_subtract = @num1 - @num2
+
+    render({ :template => "math_templates/subtract_results.html.erb"})
+  end
+
+  ###
+
+  def blank_multiply_form
+
+    render({ :template => "math_templates/multiply_form.html.erb"})
+  end
+
+  def blank_multiply_results
+
+    @num1 = params.fetch("num1").to_f
+    @num2 = params.fetch("num2").to_f
+    @result_multiply = @num1 * @num2
+
+    render({ :template => "math_templates/multiply_results.html.erb"})
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
 
+get("/add", { :controller => "application", :action => "blank_add_form"})
+
+get("/add/results", { :controller => "application", :action => "blank_add_results"})
+
+get("/subtract", { :controller => "application", :action => "blank_subtract_form"})
+
+get("/subtract/results", { :controller => "application", :action => "blank_subtract_results"})
+
+get("/multiply", { :controller => "application", :action => "blank_multiply_form"})
+
+get("/multiply/results", { :controller => "application", :action => "blank_multiply_results"})
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
